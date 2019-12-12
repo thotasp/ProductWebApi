9abf9bfec7068dbb3b896039f7eb69a1767d6a29
thotasp <sreepallavi.thota@lcginc.com>
2019-12-11 23:28:38 -0500

    naming

---
+++ ProductWebApi/ProductWebApi/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using MvcWebApi1.DataAccess;
+using MvcWebApi1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcWebApi1.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public List<ProductModel> GetAllProducts()
+        {
+            using(AdventureWorksEntities ctx = new AdventureWorksEntities())
+            {
+                var query = from p in ctx.Products select new ProductModel() { ProductId = p.ProductID, Name = p.Name, ProductNumber = p.ProductNumber, ListPrice = p.ListPrice };
+                return query.ToList();
+            } 
+        }
+
+        public ProductModel Get(int id)
+        {
+            using (AdventureWorksEntities ctx = new AdventureWorksEntities())
+            {
+                var query = from p in ctx.Products where p.ProductID == id select new ProductModel() { ProductId = p.ProductID, Name = p.Name, ProductNumber = p.ProductNumber, ListPrice = p.ListPrice };
+                return query.FirstOrDefault();
+            }
+        }
+    }
+}
